dcebd07e8172430bb8b4ee983f28926a07f49b75
cheesysamira <samiram18@nycstudents.net>
2023-09-25 18:43:20 -0400

    Initialize commit

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class RectangleRunner {
+public class RectangleRunner {
+    public static void main(String[] args) {
+
+
+// creating one instance/object from the Rectangle class
+        Rectangle rect1 = new Rectangle(5, 6);
+        rect1.printArea(); // calling a method on the object
+
+
+// creating ANOTHER instance/object from the Rectangle class
+        Rectangle rect2 = new Rectangle(10, 8);
+        rect2.printArea(); // calling a method on the object
+
+
+//rectangle 3
+        Rectangle rect3 = new Rectangle(23, 25);
+        rect3.printArea();
+    }
 }
